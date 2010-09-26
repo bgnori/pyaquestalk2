382f3c975895c37a28ae827c6d5be5a9b771b806
Ken Boo <kenbooing@gmail.com>
2010-09-26 02:17:50 -0700

    add preprocessing.  hankaku/zenkaku conversion etc

--- aquestalk2/yahoo.py
+++ aquestalk2/yahoo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib, urllib2
-from jcconv import half2wide
+from jcconv import *
+from romaji import romaji2hiragana
 import result_set
 
 YAHOO_APP_ID='F0bd9VOxg67v_QBajUI0zjDKrpC_nculhfOlHGvU6qyPQg9Pvd6uSA4H7aiK'
@@ -14,11 +15,38 @@ def call_parser(sentence):
     content = urllib2.urlopen(request).read()
     return content
 
+def preprocess(sentence):
+    """
+    ・などの記号類
+    アルファベットがだめ --> ローマ字変換導入
+                         --> それでもダメなら一文字ずつ読む
+    """
+    # ローマ字
+    sentence = romaji2hiragana(sentence)
+
+    # 記号類全部
+    for c in u'・＋123':
+        sentence = sentence.replace(c.encode('utf-8'), '、')
+    # 半角カナ
+    sentence = half2hira(sentence)
+    # 半角・全角スペース
+    sentence = sentence.replace(' ', '、')
+    sentence = sentence.replace('　', '、')
+    
+    # 半角- ハイフン
+    sentence = sentence.replace('ｰ', 'ー')
+    sentence = sentence.replace('-', 'ー')
+    # 全角ー ハイフン
+    sentence = sentence.replace('ー', 'ー')
+
+    return sentence
+
 def to_phonetic(sentence):
     """
     takes UTF-8 string
     returns UTF-8 string
     """
+    sentence = preprocess(sentence)
     xml = call_parser(half2wide(sentence))
     rs = result_set.parseString(xml)
     res = []
